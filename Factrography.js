493c1fffb601c01eaad8850a05f8c7a5dfcfd045
APonugoti1 <aponugo1@jh.edu>
2025-10-01 09:53:50 -0400

    Added js file with necessary functions

---
+++ Factrography.js
@@ -0,0 +1,39 @@
+// Autocomplete helpers
+window.JSONEditor.defaults.callbacks.autocomplete = {
+    'search_deposition': function (editor, input) {
+        if (input.length < 3) return [];
+        return restRequest({ url: 'deposition', method: 'GET', data: { q: input, limit: 10 } });
+    },
+    'render_deposition': function (editor, result, props) {
+        try {
+            const localId = result.metadata.alternateIdentifiers.find(id => id.alternateIdentifierType.toLowerCase() === 'local');
+            return `<li ${props}> ${result.igsn} (localId: ${localId.alternateIdentifier})</li>`;
+        } catch (e) {
+            return `<li ${props}> ${result.igsn} (title: ${result.metadata.titles[0]['title']})</li>`;
+        }
+    },
+    'get_deposition_value': function (editor, result) {
+        try {
+            const localId = result.metadata.alternateIdentifiers.find(id => id.alternateIdentifierType.toLowerCase() === 'local');
+            return `${result.igsn} - ${result._id} - ${localId.alternateIdentifier}`;
+        } catch (e) {
+            return `${result.igsn} - ${result._id} - no localId`;
+        }
+    }
+};
+
+// Split helper for template auto-filling
+Handlebars.registerHelper('split', function (string, separator, index) {
+    try { return string.split(separator)[index].trim(); } catch (e) { return ''; }
+});
+
+// Custom formula helpers
+Handlebars.registerHelper("sqrt", function (value) {
+  if (typeof value === "number") return Math.sqrt(value).toFixed(2);
+  return "";
+});
+
+Handlebars.registerHelper("equidiameter", function (value) {
+  if (typeof value === "number") return Math.sqrt((4 * value) / Math.PI).toFixed(2);
+  return "";
+});
