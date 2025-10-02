ff1a57d2abe7cee2d1d7431814071bc71aff19d9
APonugoti1 <aponugo1@jh.edu>
2025-10-02 10:07:32 -0400

    created factrography schema

---
+++ factrography_schema.js
@@ -0,0 +1,43 @@
+
+// Autocomplete helpers
+window.JSONEditor.defaults.callbacks.autocomplete = {
+  'search_deposition': function (editor, input) {
+    if (input.length < 3) return [];
+    return restRequest({
+      url: 'deposition',
+      method: 'GET',
+      data: { q: input, limit: 10 }
+    });
+  },
+  'render_deposition': function (editor, result, props) {
+    try {
+      const localId = result.metadata.alternateIdentifiers.find(
+        (id) => id.alternateIdentifierType.toLowerCase() === 'local'
+      );
+      return `<li ${props}>${result.igsn} (localId: ${localId.alternateIdentifier})</li>`;
+    } catch (e) {
+      return `<li ${props}>${result.igsn} (title: ${result.metadata.titles[0]['title']})</li>`;
+    }
+  },
+  'get_deposition_value': function (editor, result) {
+    try {
+      const localId = result.metadata.alternateIdentifiers.find(
+        (id) => id.alternateIdentifierType.toLowerCase() === 'local'
+      );
+      return `${result.igsn} - ${result._id} - ${localId.alternateIdentifier}`;
+    } catch (e) {
+      return `${result.igsn} - ${result._id} - no localId`;
+    }
+  }
+};
+
+// Handlebars helpers
+Handlebars.registerHelper('sqrt', function (value) {
+  try { return Math.sqrt(parseFloat(value)).toFixed(2); } 
+  catch (e) { return 0; }
+});
+
+Handlebars.registerHelper('equidiameter', function (area) {
+  try { return Math.sqrt((4 * parseFloat(area)) / Math.PI).toFixed(2); }
+  catch (e) { return 0; }
+});
